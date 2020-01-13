3f04420f5a53af293dee05e5cd0c8ac0119df7cf
Kenneth Neylon <kenneth_neylon@hotmail.com>
2020-01-13 22:02:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,25 +10,6 @@ class Song
     self.class.all << self
   end
 
-  def self.create
-    song = self.new
-  end
   
-  def self.new_by_name(name)
-    song = self.new 
-    song.name = name
-    song
-  end
-  
-  def self.create_by_name(name)
-    song = self.new 
-    song.name = name
-    @@all << song
-    song
-  end
-  
-  def self.find_by_name(name)
-    @@all.find{|x| x.name == name}
-  end
 
 end
