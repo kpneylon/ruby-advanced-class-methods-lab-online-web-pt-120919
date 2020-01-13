7de25aced1627ebe7a6299b3c69b06e02832e921
Kenneth Neylon <kenneth_neylon@hotmail.com>
2020-01-13 22:11:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,17 +33,9 @@ class Song
     self.all.detect {|i| i.name == string_name_of_the_song}
   end
   
-  def self.find_or_create_by_name(find_this_song)
-    did_i_find_it = self.all.detect {|x| x.name == find_this_song}
-    if did_i_find_it == nil
-      s = self.new 
-      s.name = find_this_song
-      s.save 
-      s
-    else
-      did_i_find_it
-    end
-  end 
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
 
   def self.alphabetical
     self.all.sort_by { |x| x.name}
